485ec48af691eb4c619cb6085c37c11da8b12230
aasimrajabali <aasim.rajabali@gmail.com>
2017-03-18 22:31:57 -0500

    Update CreditCard.py

--- Python/CreditCard.py
+++ Python/CreditCard.py
@@ -3,17 +3,7 @@
 #  Description: This program tests whether a 15 or 16-digit credit card is valid
 #               through Luhn's Algoritm, and then determines the type of credit card
 
-#  Student Name: Aasim Rajabali
-
-#  Student UT EID: afr447   
-
-#  Course Name: CS 303E
-
-#  Unique Number: 52700
-
 #  Date Created: October 20, 2014
-
-#  Date Last Modified: October 20, 2014
 
 #This function checks if a credit card number is valid
 
